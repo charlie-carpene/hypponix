71712d8ccfe3a8b505964a0978891b9f5a53101b
Maxime <maxdevvynter@gmail.com>
2025-03-10 11:16:59 +0100

    refactor: rename transitions for improved readability

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,25 +3,21 @@
 import { motion } from "framer-motion";
 
 import Link from "next/link";
-import { useMemo } from "react";
 import { FaInstagram, FaSoundcloud } from "react-icons/fa";
 
 const Hero = () => {
-  const containerVariants = useMemo(
-    () => ({
-      hidden: { opacity: 0 },
-      visible: {
-        opacity: 1,
-        transition: {
-          delayChildren: 0.3,
-          staggerChildren: 0.2,
-        },
+  const containerTransition = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2,
       },
-    }),
-    []
-  );
+    },
+  };
 
-  const itemVariants = {
+  const itemTransition = {
     hidden: { opacity: 0, x: "-50px" },
     visible: {
       opacity: 1,
@@ -48,20 +44,20 @@ const Hero = () => {
     >
       <motion.div
         className="container mx-auto px-4 sm:px-6 lg:px-8 flex items-center relative z-10"
-        variants={containerVariants}
+        variants={containerTransition}
         initial="hidden"
         animate="visible"
       >
         <div className="flex flex-col lg:flex-row items-center w-full">
-          <motion.div className="w-full mb-10 lg:mb-0" variants={itemVariants}>
+          <motion.div className="w-full mb-10 lg:mb-0" variants={itemTransition}>
             <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-extrabold text-white mb-6">
-              <motion.span className="inline-block" variants={itemVariants}>
+              <motion.span className="inline-block" variants={itemTransition}>
                 Hypponix
               </motion.span>
             </h1>
             <motion.p
               className="text-base sm:text-lg md:text-xl lg:text-2xl text-white mb-6"
-              variants={itemVariants}
+              variants={itemTransition}
             >
               The rave was built on energy,
               <br />
@@ -73,7 +69,7 @@ const Hero = () => {
             </motion.p>
             <motion.div
               className="flex flex-row justify-center md:justify-normal space-x-10 text-white"
-              variants={itemVariants}
+              variants={itemTransition}
             >
               <Link
                 href="https://soundcloud.com/hypponix"
